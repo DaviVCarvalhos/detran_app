5e528d634edfd35b0f42d54cd070a22d6b79d82e
ianporpino1 <ianporpino127@gmail.com>
2024-10-23 15:12:38 -0300

    atualizando homepage e veiculowidget

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,32 +1,31 @@
-<<<<<<< Updated upstream
-import 'package:detranapp/Pages/LoginPage.dart';
-=======
 import 'package:detranapp/Buttons/LoginButton.dart';
-import 'package:detranapp/Pages/AgendamentoPage.dart';
 import 'package:detranapp/models/Veiculo.dart';
+import 'package:detranapp/widgets/AgendamentoWidget.dart';
 import 'package:detranapp/widgets/DetranTitle.dart';
 import 'package:detranapp/widgets/InfoWidget.dart';
->>>>>>> Stashed changes
 import 'package:detranapp/widgets/InfracoesWidget.dart';
 import 'package:detranapp/widgets/LeilaoWidget.dart';
 import 'package:detranapp/widgets/VeiculoWidget.dart';
-import 'package:detranapp/widgets/AgendamentoWidget.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
-  // Lista de widgets
-  final List<Widget> widgets = [
-    VeiculoWidget(),
-    LeilaoWidget(),
-    InfracoesWidget(),
-  ];
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Veiculo> meusVeiculos = [];
+
+  void adicionarVeiculo(Veiculo veiculo) {
+    setState(() {
+      meusVeiculos.add(veiculo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< Updated upstream
-=======
     final List<Widget> widgets = [
       VeiculoWidget(
           veiculos: meusVeiculos, onVeiculoAdicionado: adicionarVeiculo),
@@ -34,41 +33,19 @@ class HomePage extends StatelessWidget {
       InfoWidget(),
       LeilaoWidget(),
       AgendamentoWidget()
+      //
     ];
 
->>>>>>> Stashed changes
     return Scaffold(
       appBar: AppBar(
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: const Color.fromARGB(
-                    255, 0, 128, 198), // Cor do texto e ícone
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(20), // Bordas arredondadas
-                ),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()),
-                );
-              },
-              icon: const Icon(Icons.login, size: 20),
-              label: const Text('LOGIN'),
-            ),
+            child: LoginButton(),
           ),
         ],
-        title: Image.asset(
-          'images/detranlogo.jpg',
-          height: 50,
-        ),
-        centerTitle: true,
+        title: DetranTitle(),
+        centerTitle: false,
         backgroundColor: const Color.fromARGB(255, 0, 128, 198),
       ),
       body: LayoutBuilder(
@@ -78,7 +55,7 @@ class HomePage extends StatelessWidget {
             child: GridView.builder(
               padding: const EdgeInsets.symmetric(vertical: 20),
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 250, // Largura máxima dos widgets aumentada
+                maxCrossAxisExtent: 200, // Largura máxima dos widgets aumentada
                 mainAxisSpacing: 20, // Espaçamento vertical entre widgets
                 crossAxisSpacing: 20, // Espaçamento horizontal entre widgets
                 childAspectRatio: 1, // Proporção mais equilibrada
@@ -86,16 +63,9 @@ class HomePage extends StatelessWidget {
               itemCount: widgets.length,
               itemBuilder: (BuildContext context, int index) {
                 return ClipRRect(
-                  borderRadius: BorderRadius.circular(
-                      15), // Define as bordas arredondadas
+                  borderRadius: BorderRadius.circular(15),
                   child: Container(
-<<<<<<< Updated upstream
-                    color: Colors
-                        .blueAccent, // Definindo uma cor para o fundo do widget
-=======
->>>>>>> Stashed changes
-                    child: widgets[
-                        index], // Widget dentro do Container arredondado
+                    child: widgets[index], // Widget dentro do Container arredondado
                   ),
                 );
               },
