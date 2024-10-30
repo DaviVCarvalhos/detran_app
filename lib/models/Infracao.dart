388bcdbdca96107c9188bf749b4e72f07e1c136c
10730684474 <davivcl@yahoo.com.br>
2024-10-30 10:07:46 -0300

    criação classe infração

---
+++ lib/models/Infracao.dart
@@ -0,0 +1,12 @@
+
+
+class Infracao{
+
+Infracao({required this.Cpf, required this.nInfracao, required this.placa, required this.renavam});
+
+String renavam; 
+String placa;
+String Cpf;
+String nInfracao;  
+  
+}
