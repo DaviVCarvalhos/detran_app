d94d66179e9177b7f90ff7648bff51678570bdb8
DaviVCarvalhos <151264268+DaviVCarvalhos@users.noreply.github.com>
2024-11-22 18:05:22 -0300

    configuracao do aplicativo com firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:detranapp/Pages/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'Pages/HomePage.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
-void main() {
-  runApp(
-     MaterialApp(
-      title: 'DetranApp',
-      home: HomePage(),
-    ),
-  );
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Detran App',
+      home: LoginPage(),
+    );
+  }
 }
